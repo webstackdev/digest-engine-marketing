a526fb471d249dc86b2144a040eb779bf15f234b
Kevin Brown <kevin@webstackbuilders.com>
2026-05-23 00:01:05 +0300

    Migrate terms content to Sanity

--- packages/sanity-schema/src/index.ts
+++ packages/sanity-schema/src/index.ts
@@ -1,7 +1,9 @@
 import compliancePage from "./compliancePage";
 import cookiesPage from "./cookiesPage";
+import termsPage from "./termsPage";
 
 export { compliancePage };
 export { cookiesPage };
+export { termsPage };
 
-export const schemaTypes = [compliancePage, cookiesPage];
+export const schemaTypes = [compliancePage, cookiesPage, termsPage];
